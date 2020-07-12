900bb7e357dc9a290292c0d28b013520c75bdc46
Crosson David <crosson.david@gmail.com>
2020-07-12 10:56:40 +0200

    Enhance tests when no remote publish target is defined

--- src/test/scala/fr/janalyse/cem/externalities/publishadapter/github/GithubPublishAdapterTest.scala
+++ src/test/scala/fr/janalyse/cem/externalities/publishadapter/github/GithubPublishAdapterTest.scala
@@ -14,22 +14,23 @@ class GithubPublishAdapterTest extends AnyFlatSpec with should.Matchers {
   // Those tests are only executed with a github token is available
 
   Configuration().publishAdapters.get("github-com-gists").foreach { config =>
-    val adapter = new GithubPublishAdapter(config)
+    GithubPublishAdapter.lookup(config).collect { case adapter:GithubPublishAdapter =>
 
-    "GithubPublishAdapter" should "be able to get authenticated user information" in {
-      adapter.getUser() shouldBe defined
-    }
+      "GithubPublishAdapter" should "be able to get authenticated user information" in {
+        adapter.getUser() shouldBe defined
+      }
 
-    it should "be able to list available gists" in {
-      val user = adapter.getUser().value
-      val gists = adapter.userGists(user)
-      info(s"found ${gists.size} gist for user ${user.login}")
-      gists.size shouldBe > (0)
-    }
+      it should "be able to list available gists" in {
+        val user = adapter.getUser().value
+        val gists = adapter.userGists(user)
+        info(s"found ${gists.size} gist for user ${user.login}")
+        gists.size shouldBe >(0)
+      }
 
-    it should "be able to publish a code example" in {
-      val example = CodeExample(pwd / "test-data" / "sample1" / "fake-testing-pi.sc", pwd / "test-data" / "sample1")
-      adapter.synchronize(example::Nil)
+      it should "be able to publish a code example" in {
+        val example = CodeExample(pwd / "test-data" / "sample1" / "fake-testing-pi.sc", pwd / "test-data" / "sample1")
+        adapter.synchronize(example :: Nil)
+      }
     }
   }
 }
