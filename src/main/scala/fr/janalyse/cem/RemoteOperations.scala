cf8b6d397d7b49ec942e6049b1591060bfa5dfce
Crosson David <crosson.david@gmail.com>
2021-04-18 16:02:44 +0200

    [ci-skip] Manage github graphql gists pagination

--- src/main/scala/fr/janalyse/cem/RemoteOperations.scala
+++ src/main/scala/fr/janalyse/cem/RemoteOperations.scala
@@ -6,6 +6,7 @@ import zio.RIO
 import zio.logging._
 import sttp.client3._
 import sttp.client3.asynchttpclient.zio._
+import zio.stream.ZStream
 
 
 object RemoteOperations {
@@ -24,15 +25,15 @@ object RemoteOperations {
   case class RemoteGistConnection(gists: List[RemoteGist], totalCount: Int, pagination: RemoteGistPagination)
   case class RemoteGistPagination(endCursor:Option[String], hasNextPage:Boolean)
 
-  val gistQuery = {
-    import fr.janalyse.cem.graphql.github.GitHubClient._
-    import fr.janalyse.cem.graphql.github.GitHubClient.PageInfo._
-    import fr.janalyse.cem.graphql.github.GitHubClient.User.gists
-    import fr.janalyse.cem.graphql.github.GitHubClient.Query.viewer
-    import fr.janalyse.cem.graphql.github.GitHubClient.GistConnection._
+  def gistQuery(after:Option[String]=None, first:Option[Int]=Some(100) ) = {
+    import fr.janalyse.cem.graphql.github.Client._
+    import fr.janalyse.cem.graphql.github.Client.PageInfo._
+    import fr.janalyse.cem.graphql.github.Client.User.gists
+    import fr.janalyse.cem.graphql.github.Client.Query.viewer
+    import fr.janalyse.cem.graphql.github.Client.GistConnection._
 
     viewer(
-      gists(first=Some(100), orderBy=Some(GistOrder(direction=OrderDirection.DESC, field=GistOrderField.CREATED_AT)))(
+      gists(first=first, after= after, orderBy=Some(GistOrder(direction=OrderDirection.DESC, field=GistOrderField.CREATED_AT)))(
         (
             nodes((Gist.id ~ Gist.description).mapN(RemoteGist)) ~
             totalCount ~
@@ -50,19 +51,28 @@ object RemoteOperations {
       .map(m => (m.group(1), m.group(2)))
   }
 
+  def githubRemoteExamplesStatesFetch(adapterConfig: PublishAdapterConfig, after:Option[String]=None):RIO[Logging with SttpClient, List[RemoteExampleState]] = {
+    val query = gistQuery(after).toRequest(uri"https://api.github.com/graphql", useVariables = true)
+    for {
+      response <- send(injectAuthToken(query, adapterConfig.token)).map(_.body).absolve
+      nextResults <-
+        if (response.pagination.hasNextPage) githubRemoteExamplesStatesFetch(adapterConfig, response.pagination.endCursor)
+        else RIO.succeed(List.empty[RemoteExampleState])
+      gists = for {
+        gist <- response.gists
+        desc <- gist.description
+        (uuid,checksum) <- extractMetaDataFromDescription(desc)
+      }  yield RemoteExampleState(remoteId = gist.id, description = desc, uuid=uuid, checksum = checksum)
+    } yield gists:::nextResults
+  }
+
   def remoteExampleStatesFetch(adapterConfig: PublishAdapterConfig): RIO[Logging with SttpClient, Iterable[RemoteExampleState]] = {
     for {
-      _ <- log.info(s"checking ${adapterConfig.kind}/${adapterConfig.activationKeyword}")
-      if adapterConfig.kind == "github"
-      graphqlAPI = uri"https://api.github.com/graphql"
-      query = gistQuery.toRequest(graphqlAPI, useVariables = true)
-      response <- send(injectAuthToken(query, adapterConfig.token)).map(_.body).absolve
-      _ <- log.info("Gists count "+response.totalCount)
-      // TODO manage pagination using gists(after=Some("xx")) && pagination.endCursor
-    } yield for {
-      gist <- response.gists
-      desc <- gist.description
-      (uuid,checksum) <- extractMetaDataFromDescription(desc)
-    }  yield RemoteExampleState(remoteId = gist.id, description = desc, uuid=uuid, checksum = checksum)
+      _ <- log.info(s"Checking ${adapterConfig.kind}/${adapterConfig.activationKeyword}")
+      states <-
+        if (adapterConfig.kind == "github") githubRemoteExamplesStatesFetch(adapterConfig)
+        else if (adapterConfig.kind == "gitlab") RIO.succeed(List.empty[RemoteExampleState])
+        else RIO.succeed(List.empty[RemoteExampleState])
+    } yield states
   }
 }
--- src/main/scala/fr/janalyse/cem/Synchronize.scala
+++ src/main/scala/fr/janalyse/cem/Synchronize.scala
@@ -75,7 +75,7 @@ object Synchronize {
     else {
       for {
         remoteStates <- remoteExampleStatesFetcher(adapterConfig)
-        _ <- log.info(s"found ${remoteStates.size} already published examples on ${adapterConfig.activationKeyword}")
+        _ <- log.info(s"Found ${remoteStates.size} examples already published examples on ${adapterConfig.kind}/${adapterConfig.activationKeyword}")
       } yield ()
     }
   }
